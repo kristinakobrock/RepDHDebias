c0f25fbce0b891bb101c2fdfe6dcc545d64b01fb
Sonja Boergerding <sboergerding@uni-osnabrueck.de>
2021-03-17 13:29:37 +0100

    WEAT from utils

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from sklearn import metrics as sk_m
+import sklearn
 import random as random
 from scipy.stats import norm
 import statistics as stat
@@ -201,5 +201,5 @@ class weat(object):
     def cosineSimilarity(self,a, b):
         a = [a]
         b = [b]
-        r = sk_m.pairwise.cosine_similarity(a,b)
+        r = sklearn.metrics.pairwise.cosine_similarity(a,b)
         return r[0][0]
